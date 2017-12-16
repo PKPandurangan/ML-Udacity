3ced72150f242885239b90f37bf5c3d02a5efadb
Praveen Kumar Pandurangan <praveen@arivuventures.com>
2017-12-16 09:03:57 +0000

    Final version of the file

--- 18.GaussianNBExample.py
+++ 18.GaussianNBExample.py
@@ -1,15 +1,18 @@
+#18.GaussianNBExample.py
+
+import sklearn.naive_bayes
+
 import numpy as np
 
 X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
 Y = np.array([1, 1, 1, 2, 2, 2])
-from sklearn.naive_bayes import GaussianNB
 
-clf = GaussianNB()
+clf = sklearn.naive_bayes.GaussianNB()
 clf.fit(X, Y)
-GaussianNB(priors=None)
+sklearn.naive_bayes.GaussianNB(priors=None)
 print(clf.predict([[-0.8, -1]]))
 
-clf_pf = GaussianNB()
+clf_pf = sklearn.naive_bayes.GaussianNB()
 clf_pf.partial_fit(X, Y, np.unique(Y))
-GaussianNB(priors=None)
+sklearn.naive_bayes.GaussianNB(priors=None)
 print(clf_pf.predict([[-0.8, -1]]))
